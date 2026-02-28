195960197e6929bbb84bd095208714c07f1c1ac6
alshahriar-dev <sahariarsikder.aa@gmail.com>
2026-02-28 16:09:06 +0600

    learn js scope

---
+++ 01_function.js
@@ -0,0 +1,19 @@
+function sayMyName() {
+  console.log("s");
+  console.log("h");
+  console.log("a");
+  console.log("h");
+  console.log("r");
+  console.log("i");
+  console.log("a");
+  console.log("r");
+}
+// sayMyName()
+
+const myNewArray = [200, 300, 450, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+
---
+++ 02_scope.js
@@ -0,0 +1,10 @@
+
+if (true) {
+    let a = 10
+    const b = 34
+    var c = 49
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
