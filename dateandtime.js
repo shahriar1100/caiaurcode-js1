61e7dd1aca548237c40a368e24da90b1acf669c3
alshahriar-dev <sahariarsikder.aa@gmail.com>
2026-02-28 15:14:02 +0600

    js time show

--- dateandtime.js
+++ dateandtime.js
@@ -1,2 +1,6 @@
-let name = 'shahriar'
-console.log(name);
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
